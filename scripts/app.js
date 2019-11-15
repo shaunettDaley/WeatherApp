a97b64f69a331b8acee1295828564540edd297ec
shaunettDaley <54638418+shaunettDaley@users.noreply.github.com>
2019-11-14 23:40:00 -0500

    Upload files for weather app

---
+++ scripts/app.js
@@ -0,0 +1,84 @@
+const form = document.querySelector('form');
+const cardWrapper = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+
+const updateUI = (data) => {
+   
+    //const citydet = data.cityDet;
+    //const weather = data.weather;
+
+    //using destructing to get properites from the object
+    const {cityDet,weather} = data;
+//console.log(weather);
+    details.innerHTML = 
+    `<h5 class="my-3">${cityDet.EnglishName}</h5>
+    <div class="my-3">${weather[0].WeatherText}</div>
+    <div class="display-4 my-4">
+            <span>${weather[0].Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+    </div>`;
+
+    //show icon
+    let iconSrc = `img/icons/${weather[0].WeatherIcon}.svg`;
+    icon.setAttribute('src',iconSrc);
+
+    //showing the time 
+    /*
+    let timeSrc = null;
+    if(weather[0].isDayTime){
+        timeSrc = "img/day.svg";
+    }else{
+        timeSrc = "img/night.svg";
+    }*/
+
+    //ternary operator
+    let timeSrc = weather[0].isDayTime ? 'img/day.svg' : 'img/night.svg';
+
+    time.setAttribute('src',timeSrc);
+
+    if(cardWrapper.classList.contains('d-none')){
+        cardWrapper.classList.remove('d-none');
+    }
+};
+
+const updateCity = async (city) => {
+console.log(city);
+const cityDet = await getCity(city);
+const weather = await getWeather(cityDet.Key);
+return { cityDet, weather};
+
+
+};
+
+
+
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+    const city = form.city.value.trim();
+    form.reset();
+
+    //update UI
+    updateCity(city).then(data => {
+       updateUI(data);
+    }).catch(err => {
+        console.log(err);
+    })
+
+    //store info in local storage
+    localStorage.setItem('city',city);
+});
+
+
+//run this function each time the browser refresh or start
+if(localStorage.getItem('city')){
+    const localInfo = updateCity(localStorage.getItem('city'))
+.then(data => {
+    updateUI(data);
+}).catch(err => {
+    console.log(err);
+})
+
+}
---
+++ scripts/forecast.js
@@ -0,0 +1,32 @@
+const key = "zIFNxdWjH8AP61zf0AuoPTC7mZy5IwT2";
+
+//get weather information
+const getWeather = async (keycode) =>{
+    const weatherBase = "http://dataservice.accuweather.com/currentconditions/v1/"
+    const query2 = `${keycode}?apikey=${key}`;
+    const response1 = await fetch(weatherBase + query2);
+    const data2 = await response1.json();
+  console.log(data2);
+   return data2;
+}
+
+//get city information
+const getCity = async (city) => {
+    const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const query = `?apikey=${key}&q=${city}`;
+    const response = await fetch(base + query);
+    const data = await response.json();
+    console.log(data);
+    return data[0];
+
+}
+
+/*
+getCity('manchester').then(data => {
+  return getWeather(data.Key);
+}).then(data => {
+      console.log(data);
+  }).catch(err => console.log(err));
+*/
+
+//getWeather('329260');
